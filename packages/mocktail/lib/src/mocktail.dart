5b893b02c6c7e2c0280df78cc614d3d69ffba736
Sebastien Varlet <sebastien.varlet@gmail.com>
2021-02-11 16:14:05 +0000

    Replace assert by throw

--- packages/mocktail/lib/src/mocktail.dart
+++ packages/mocktail/lib/src/mocktail.dart
@@ -555,10 +555,12 @@ class _StubFunction extends _MockInvocationCall {
   }
 
   void thenReturn([Object? result]) {
-    assert(!(result is Future), '''
+    if (result is Future) {
+      throw ArgumentError('''
       It is highly discouraged to call `.thenReturn` with a Future.
       Use `thenAnswer` instead.
       ''');
+    }
     _object._stubs[_invocation] = _Stub((_) => result);
   }
 
